7a00c44775354a3fa115e646488e3b88caea9f87
martinjoseb <jose.martin.barrutia@ibm.com>
2025-03-18 16:29:08 -0500

    subida

--- application/controllers/Procesos.php
+++ application/controllers/Procesos.php
@@ -79,6 +79,70 @@ class Procesos extends BaseController {
 			}
 		}
 
+
+    public function proceso_envio_boleta_fecha(){
+    // Obtener la fecha desde el parámetro GET, si no se envía se usa la fecha actual
+    $fecha = $this->input->get('fecha');
+    if (empty($fecha)) {
+        $fecha = date("Y-m-d");
+    }
+
+    // Definir la URL y el token (en producción)
+    $ruta = 'https://factura.selvafood.com/api/summaries';
+    $token = '8qn5JHCUSsWkm2Q7hemQ57HNCDV0t4n0PE8iSIvT5cBuLfphmC';
+
+    // Preparar los datos para el POST
+    $data = array();
+    $data["fecha_de_emision_de_documentos"] = $fecha;
+    $data["codigo_tipo_proceso"] = "1";
+    $data_json = json_encode($data);
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $ruta,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => $data_json,
+        CURLOPT_HTTPHEADER => array(
+            "authorization: Bearer " . $token,
+            "cache-control: no-cache",
+            "content-type: application/json",
+        ),
+    ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        $json = json_decode($response, true);
+        print_r($json);
+        if ($json["success"] == 1) {
+            $data_update = array(
+                "venta_resumen_external_id" => $json["data"]["external_id"],
+                "venta_resumen_ticket" => $json["data"]["ticket"],
+                "venta_resumen_fecha" => date("Y-m-d H:i:s")
+            );
+             
+            // Utiliza el parámetro fecha en la condición de actualización
+            $sql = "date(venta.venta_pedidofecha)='" . $fecha . "' and (venta_resumen_external_id is null or venta_resumen_external_id='') and ventas_idtipodocumento=2";
+            $this->db->where($sql);
+            $r = $this->db->update("venta", $data_update);
+        }
+    }
+}
+
+
+
+
+
 public function verificar_envio_boleta()
 {
 
